419f8b609ef41e56948511a111803526b0dec5e6
giovanniGarcia22 <alu.23130614@correo.itlalaguna.edu.mx>
2025-02-18 13:53:48 -0600

    Add files via upload

---
+++ Practica3/Practica3/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Practica3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            butAbrir = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Highlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 53);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 385);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // butAbrir
+            // 
+            butAbrir.Font = new Font("Perpetua Titling MT", 18F, FontStyle.Bold);
+            butAbrir.Location = new Point(12, 15);
+            butAbrir.Name = "butAbrir";
+            butAbrir.Size = new Size(110, 32);
+            butAbrir.TabIndex = 2;
+            butAbrir.Text = "Abrir";
+            butAbrir.UseVisualStyleBackColor = true;
+            butAbrir.Click += butAbrir_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|Excel (*.csv*)|*.csv*";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(butAbrir);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button butAbrir;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Practica3/Practica3/Form1.cs
@@ -0,0 +1,57 @@
+using System.Windows.Forms;
+
+namespace Practica3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void butAbrir_Click(object sender, EventArgs e)
+        {
+            String nomFile = "";
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                nomFile = openFileDialog1.FileName;
+                try
+                {
+                    string read = File.ReadAllText(nomFile);
+                    string[] aux1 = read.Split('\n');
+                    string[] aux2 = aux1[0].Split(',');
+                    int tam = aux2.Length;
+                    dataGridView1.ColumnCount = tam;
+                    dataGridView1.RowCount = aux1.Length;
+                    for (int i = 0; i < tam; i++)
+                    {
+                        dataGridView1.Rows[0].Cells[i].Value = aux2[i];
+                    }
+
+                    for (int i = 1; i < (aux1.Length) - 1; i++)
+                    {
+                        aux2 = aux1[i].Split(',');
+                        for (int j = 0; j < tam; j++)
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = aux2[j];
+                        }
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el archivo: " + ex.Message);
+                }
+            }
+
+
+        }
+    }
+}
